570a7ac3afb8c66d0e7955cd9742702beb20ba13
Andy Hong <48839179+andyhong0122@users.noreply.github.com>
2022-05-17 13:56:39 -0700

    Time: 77 ms (65.02%), Space: 45.3 MB (34.75%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,20 +4,25 @@
  * @return {number}
  */
 
-const search = (nums, target) => {
-  let left = 0;
-  let right = nums.length - 1;
+/// Binary search
 
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2); // ! getting out mid val
-    if (nums[mid] === target) { // ! if mid val is equal to target we will return the mid 
-      return mid;
-    } else if (nums[mid] < target) { 
-      left = mid + 1; /// ! if mid val is less than target we will move the left pointer to mid+1
-    } else {
-        right = mid - 1; // ! if mid val is greater than target we will move the right pointer mid-1
+// O(log n)
+
+
+const search = (nums, target) => {
+    let l = 0;
+    let r = nums.length - 1;
+    
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        if (nums[m] === target) {
+            return m;
+        } else if (nums[m] > target) {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
     }
-  }
-  //! if we cant find the target we will return -1
-  return -1;
-};
+    
+    return -1;
+}
