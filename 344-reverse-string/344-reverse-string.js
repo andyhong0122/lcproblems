3791c87a3df5b695f32dc89d38c8101dd24a1bc0
Andy Hong <48839179+andyhong0122@users.noreply.github.com>
2022-05-22 20:17:13 -0700

    Time: 88 ms (94.19%), Space: 49.6 MB (28.77%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+
+// O(n), visit each char
+// O(1), no extra space
+var reverseString = function(s) {
+    let l = 0;
+    let r = s.length - 1;
+    
+    while (l < r) {
+        let temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        l++;
+        r--;
+    }
+    
+    return s;
+};
