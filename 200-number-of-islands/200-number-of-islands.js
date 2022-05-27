3fe16674e428b1bda40062f0a00b8db22fb915d1
Andy Hong <48839179+andyhong0122@users.noreply.github.com>
2022-05-27 00:19:23 -0700

    Time: 119 ms (49.95%), Space: 50.5 MB (29.84%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,41 +3,8 @@
  * @return {number}
  */
 
-// bfs
-var numIslands = function(grid) {
-    let width = grid[0].length;
-    let height = grid.length;
-    let counter = 0;
-    let dir = [[0,1],[1,0],[-1,0],[0,-1]]
-    
-    for(let row = 0; row < height; row++) {
-        for(let col = 0; col < width; col++) {
-            if(grid[row][col] === "1") {
-                counter++;
-                // when we encounter a "1", trigger a dfs
-                visit([row, col]) 
-            }
-        }
-    }
-    
-    function visit(pair) {
-        let q = [pair]
-        while(q.length) {
-            let [row, col] = q.shift();
-            let isOutofBounds = row < 0 || col < 0 || row >= height || col >= width;
-            if(isOutofBounds || grid[row][col] === "0") continue;
-            
-            grid[row][col] = "0";
-            
-            for(let d of dir) {
-                q.push([row + d[0], col + d[1]]);
-            }
-        }
-    }
-    
-    return counter;
-};
-
+// O(m*n), we traverse through each coordinates exactly once
+// O(m*n), in worst case, (which is what we want to list as complexity), the grid is filled with lands, so the size of the queue can grow up to m*n
 // bfs
 var numIslands = function(grid) {
     // declare row and col sizes
