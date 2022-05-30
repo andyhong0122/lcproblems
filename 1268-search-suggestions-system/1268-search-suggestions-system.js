1c3f4f02b2adf2819496c5a034b1298cc471455e
Andy Hong <48839179+andyhong0122@users.noreply.github.com>
2022-05-30 06:20:25 -0700

    Time: 184 ms (51.33%), Space: 53 MB (74.33%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.js
+++ 1268-search-suggestions-system/1268-search-suggestions-system.js
@@ -8,6 +8,8 @@
 
 var suggestedProducts = function(products, searchWord) { 
     if(!searchWord) return []
+    
+    // because we sorted products already here, our list will return in lexicographical order
     products.sort(); 
     
     let list = [];
@@ -20,8 +22,13 @@ var suggestedProducts = function(products, searchWord) {
         
         // search through all products (remember they're sorted)
         for (let j = 0; j < products.length; j++) {
-            if(!(suggestions.length === 3) && products[j].substring(0, i + 1) === curr) suggestions.push(products[j]);
+            // if suggestions count is not 3 (yet) and if the current product substring matches our search word, push into suggestions list
+            if(!(suggestions.length === 3) && products[j].substring(0, i + 1) === curr) {
+                suggestions.push(products[j]);   
+            }
         }
+        
+        // at the end of our products search, whether we have found our max number of suggestions (3) or we have found nothing, add suggestions to list
         list.push(suggestions);
         suggestions = [];
     }
