6d1ef664b6340d4368260ea2bb1f8263ca0e09b4
Andy Hong <48839179+andyhong0122@users.noreply.github.com>
2022-05-30 04:39:51 -0700

    Time: 134 ms (53.19%), Space: 52.3 MB (91.49%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ */
+
+// two pointers
+
+// o(n), iterate over colors once
+var minCost = function(colors, neededTime) {
+    if (colors.length === 1) return 0;
+    
+    let total = 0;
+    let l = 0;
+    let r = 1;
+    
+    while (r < colors.length) { 
+        if (colors[l] !== colors[r]) {
+            l = r; 
+        } else {
+            total += Math.min(neededTime[l], neededTime[r]);
+            
+            if (neededTime[l] < neededTime[r]) {
+                l = r;
+            }
+        }
+        
+        r++;
+    }
+    
+    return total;
+};
+
+// using two pointers, traverse through the array
+// if we find two consecutive balloons, remove the one with smaller time
