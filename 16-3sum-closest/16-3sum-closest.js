efee1374a6f2843e313fa75269036925ffd16e0f
Andy Hong <48839179+andyhong0122@users.noreply.github.com>
2022-06-03 04:01:43 -0700

    Time: 89 ms (62.74%), Space: 42.7 MB (59.97%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.js
+++ 16-3sum-closest/16-3sum-closest.js
@@ -4,18 +4,27 @@
  * @return {number}
  */
 
+// two pointers
+
+// o(n^2), inner and outer loop which grows with input size
+// o(log n), sorting algorithm uses auxiliary space
 var threeSumClosest = function(nums, target) {
-    nums.sort((a,b)=>a-b);
+    nums.sort((a, b) => a - b);
     let closest = Infinity;
     
-    for (let i=0;i<nums.length-2;i++) {
-        let left = i+1; right = nums.length-1;
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1; 
+        let right = nums.length - 1;
+        
         while (left < right) {
             let localSum = nums[i] + nums[left] + nums[right];
-            if (Math.abs(localSum - target) < Math.abs(closest - target)) closest = localSum;
+            if (Math.abs(localSum - target) < Math.abs(closest - target)){
+                closest = localSum;                
+            }
             if (localSum > target) right--;
             else left++;
         }
     }
     return closest;
 };
+
