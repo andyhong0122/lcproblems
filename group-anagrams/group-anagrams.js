76b27f2f6e7c80fd1a20862d4d00220aeff6dd41
Andy Hong <48839179+andyhong0122@users.noreply.github.com>
2022-06-03 04:04:14 -0700

    Time: 201 ms, Memory: 54 MB - LeetHub

---
+++ group-anagrams/group-anagrams.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+// if a word is an anagram, when sorted should be same
+
+
+// o(n * n log n), for each word, we're using sort
+// o(m * n), for array of strings, m strings of size to n at most
+var groupAnagrams = function(strs) {
+    let dict = {};
+    
+    for (let s of strs) {
+        let key = s.split("").sort().join("");
+        
+        if (dict[key]) {
+            dict[key].push(s);
+        } else {
+            dict[key] = [s];
+        }
+    }
+    
+    return Object.values(dict);
+}
+
+
+// using charCodeAt()
+// o(m * n), where  is number of words and n is the length of each string
+// o(m * n), the size of the dictionary we're using
+var groupAnagrams = function(strs) {
+    let dict = {};
+    
+    for (let word of strs) {
+        const arr = new Array(26).fill(0);
+        
+        for (let char of word) {
+            let index = char.charCodeAt() - 97;
+            arr[index]++;
+        }
+
+        let key = arr.join("#");
+        if (dict[key]) {
+            dict[key].push(word);
+        } else {
+            dict[key] = [word] ;
+        }
+    }
+    
+    return Object.values(dict);
+}
+
