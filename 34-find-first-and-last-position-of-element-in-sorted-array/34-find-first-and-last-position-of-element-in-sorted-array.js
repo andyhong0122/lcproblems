a1ed9d1282d4ee48646e9c536d18380ff6aa837e
Andy Hong <48839179+andyhong0122@users.noreply.github.com>
2022-06-08 08:21:29 -0700

    Time: 85 ms (44.00%), Space: 42.3 MB (83.23%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+/*
+BS; if mid is target, then start two pointers from there, "start" and "end"
+"start" will -- as we see same number
+"end" will ++ as we see same number
+assign start and end to result as tuple
+*/
+
+var searchRange = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    let result = [-1, -1]
+    
+    while(left <= right){
+        let mid = Math.floor(right - left / 2) + left;
+        let start = mid;
+        let end = mid;
+        
+        if(target === nums[mid]){
+            while(nums[start] === nums[start - 1]) start--
+            while(nums[end] === nums[end + 1]) end++
+            result[0] = start;
+            result[1] = end;
+            return result;
+        }
+        
+        if(target < nums[mid]){
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    return result;
+};
